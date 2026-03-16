c22519f4cdbc3ca3a57b1853fb0e49139d8e98b9
uniqcarz <uniqcars21@gmail.com>
2026-03-16 14:43:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,14 +7,24 @@ let price = document.getElementById("price").value;
 let currentYear = new Date().getFullYear();
 let age = currentYear - year;
 
-let depreciation = price * (0.12 * age);
+let depreciationFactor;
 
-let kmsPenalty = kms * 0.3;
+if(age <= 1) depreciationFactor = 0.80;
+else if(age == 2) depreciationFactor = 0.70;
+else if(age == 3) depreciationFactor = 0.60;
+else if(age == 4) depreciationFactor = 0.50;
+else if(age == 5) depreciationFactor = 0.40;
+else depreciationFactor = 0.30;
 
-let value = price - depreciation - kmsPenalty;
+let value = price * depreciationFactor;
 
-if(value < price * 0.25){
-value = price * 0.25;
+// km adjustment
+if(kms > 80000){
+value = value * 0.90;
+}
+
+if(kms > 120000){
+value = value * 0.80;
 }
 
 document.getElementById("result").innerHTML =
